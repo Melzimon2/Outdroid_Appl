5ba2e3d5188ab0151b12c09ee31a672a88665b87
Melzimon2 <melxymenkeithplazos@gmail.com>
2026-04-18 11:37:22 +0000

    Added Hardware OS and completed Firmware bridge connector

---
+++ Kernel_Starter/OS_Entry/Hardware_OS.java
@@ -0,0 +1,13 @@
+package OS_Entry;
+
+public class Hardware_OS {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+    private static class official_settings {
+        //The official settings code
+        //Only accessed during Hardware optimization
+        
+
+    }
+}
--- Kernel_Starter/firmware_bridge.java
+++ Kernel_Starter/firmware_bridge.java
@@ -16,10 +16,43 @@ public class firmware_bridge {
       System.exit(1);
       }
       try (FileWriter writer = new FileWriter("FirmwareBridgerOS.log")) {
-        writer.write("hello");
+        //creates a BridgeOS Log
+        writer.write("Firmware: Official\n");
+        writer.write("ClassBridge: Connected\n");
+        writer.write("Exception: none\n");
       } catch (IOException e) {
         e.printStackTrace();
       }
-
+      try (FileWriter writer = new FileWriter("Optimization.log")) {
+        // Where the user could see the optimization of the firmware
+        // Not really available but mostly is optimized to avoid errors
+        writer.write("Optimization Setting MaxVar: " + Optimization.optimization_setting_var + "\n");
+        writer.write("Config libs set to: " + Optimization.config_libs + "\n");
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      
     }
+    private static class Optimization {
+      static final String optimization_setting_var = "64";
+      static final boolean config_libs = true;
+      // configuration libs are set to true incase of libraries
+    }
+    public static void final_hardware() {
+      // Checking if hardware os does exist
+      // final step for the firmware bridge
+      Path dir_hardware = Paths.get("Hardware_OS.java");
+      if (Files.exists(dir_hardware)) {
+        System.out.print("");
+      } else {
+        System.out.print("");
+        System.exit(4);
+      }
+    }
+    private static class ERR_MES {
+      static final String err_hardware = "[OS] [SYS] Couldn't find Hardware OS files! Return 4;";
+      // Firmware faulty happens
+      static final String Firmware_faulty = "";
+    }
+    
 }
